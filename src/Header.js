10250ff9bba41f6c4bf31fed68a93d2d2b1355af
jiasir <jiasir@icloud.com>
2023-04-23 22:49:06 +0800

    Upgrade to bootstrap version 5.3.0

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,24 @@
 function Header() {
     return (
-        <header className="p-3 text-bg-dark mb-auto">
-            <div className="container">
-                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                    <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                        <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                            <use xlinkHref="#bootstrap"></use>
-                        </svg>
-                    </a>
+        <div className="container mb-auto">
+            <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                <a href="/"
+                   className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                    <svg className="bi me-2" width="40" height="32">
+                        <use xlinkHref="#bootstrap"></use>
+                    </svg>
+                    <span className="fs-4">Simple header</span>
+                </a>
 
-                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">About</a></li>
-                    </ul>
-
-                    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                        <input type="search" className="form-control form-control-dark text-bg-dark"
-                               placeholder="Search..." aria-label="Search"></input>
-                    </form>
-
-                    <div className="text-end">
-                        <button type="button" className="btn btn-outline-light me-2">Login</button>
-                        <button type="button" className="btn btn-warning">Sign-up</button>
-                    </div>
-                </div>
-            </div>
-        </header>
+                <ul className="nav nav-pills">
+                    <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+                </ul>
+            </header>
+        </div>
     );
 }
 
