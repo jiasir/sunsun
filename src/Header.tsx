1b01c1e673a45526704366a3d24ceae84bd0c531
jiasir <jiasir@icloud.com>
2023-04-28 22:19:49 +0800

    Reformat code

--- src/Header.tsx
+++ src/Header.tsx
@@ -12,7 +12,8 @@ function Header(): JSX.Element {
         <div className="container mb-auto">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                 <Link to="/"
-                      className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" onClick={() => handleClick("/")}>
+                      className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+                      onClick={() => handleClick("/")}>
                     <svg className="bi me-2" width="40" height="32">
                         <use xlinkHref="#bootstrap"></use>
                     </svg>
