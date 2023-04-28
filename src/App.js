ec0b5230f9fe4a340433decb34ffb261f2dd2287
jiasir <jiasir@icloud.com>
2023-04-28 17:28:39 +0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
 import Header from "./Header";
-import Main from "./Main";
 import Footer from "./Footer";
+import Main from "./Main";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Features from "./Features";
+import Pricing from "./Pricing";
+import FAQs from "./FAQs";
+import About from "./About";
+
+
 
 
 function App() {
     return (
-        <div className="d-flex flex-column min-vh-100">
-            <Header/>
-            <Main/>
-            <Footer/>
-        </div>
+        <BrowserRouter>
+            <div className="d-flex flex-column min-vh-100">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/features" element={<Features/>}/>
+                    <Route path="/pricing" element={<Pricing/>}/>
+                    <Route path="/faqs" element={<FAQs/>}/>
+                    <Route path="/about" element={<About/>}/>
+                </Routes>
+                <Footer />
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,43 @@
+import {Link} from "react-router-dom";
+
 function Header() {
     return (
         <div className="container mb-auto">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/"
-                   className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <Link to="/"
+                      className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                     <svg className="bi me-2" width="40" height="32">
                         <use xlinkHref="#bootstrap"></use>
                     </svg>
                     <span className="fs-4">Simple header</span>
-                </a>
+                </Link>
 
                 <ul className="nav nav-pills">
-                    <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link active" aria-current="page">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/features" className="nav-link">
+                            Features
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/pricing" className="nav-link">
+                            Pricing
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/faqs" className="nav-link">
+                            FAQs
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/about" className="nav-link">
+                            About
+                        </Link>
+                    </li>
                 </ul>
             </header>
         </div>
