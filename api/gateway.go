a42d8f715c3fc145cb5171ab57fd7bda674978c9
jiasir <jiasir@icloud.com>
2023-05-04 11:01:52 +0800

    Add gateway.go

---
+++ api/gateway.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var APIKEY = os.Getenv("OPENAI_API_KEY")
+
+func main() {
+	// Create Gin instance
+	r := gin.Default()
+
+	// Define route, proxy OpenAI API
+	r.POST("/v1/chat/completions", func(c *gin.Context) {
+		// extract OpenAI API URL
+		// e.g https://api.openai.com/v1/chat/completions
+		targetUrl, err := url.Parse("https://api.openai.com")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create reverse proxy, and forward request to OpenAI API
+		proxy := httputil.NewSingleHostReverseProxy(targetUrl)
+		// Set proxy request header, add the key saved in gateway to request header
+		c.Request.Header.Set("Authorization", APIKEY)
+		proxy.ServeHTTP(c.Writer, c.Request)
+	})
+
+	// Launch Gin instance, and listen on port 8080 with TLS
+	if err := r.RunTLS(":8080", "cert.pem", "key.pem"); err != nil {
+		log.Fatal(err)
+	}
+}
