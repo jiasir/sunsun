bea45bc0ce043c4df599bc244b530f924275d2bf
jiasir <jiasir@icloud.com>
2023-05-11 09:12:02 +0800

    Using package instead of link

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+// Adding bootstrap https://create-react-app.dev/docs/adding-bootstrap/
+import 'bootstrap/dist/css/bootstrap.css'
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles. e.g. import './index.css';
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css'; // DO NOT USE THIS css file, because it affects the font
+import './index.css'; // DO NOT USE THIS css file, because it affects the font
 import App from './App';
 // @ts-ignore
 import reportWebVitals from './reportWebVitals';
-// Adding bootstrap https://create-react-app.dev/docs/adding-bootstrap/
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
