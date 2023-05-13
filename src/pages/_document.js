9dc884046c5183cefbc750b7ccfecf8efd96a353
jiasir <jiasir@icloud.com>
2023-05-13 14:38:28 +0800

    Fix path

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,10 +5,10 @@ class MyDocument extends Document {
         return (
             <Html lang="en" data-bs-theme="dark">
                 <Head>
-                    <link rel="icon" href="%PUBLIC_URL%/favicon.ico"/>
-                    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png"/>
-                    <link rel="manifest" href="%PUBLIC_URL%/manifest.json"/>
-                    <script src="colorModeToggler.js"></script>
+                    <link rel="icon" href="/favicon.ico"/>
+                    <link rel="apple-touch-icon" href="/logo192.png"/>
+                    <link rel="manifest" href="/manifest.json"/>
+                    <script src="/theme.js"></script>
                 </Head>
                 <body>
                     <noscript>You need to enable JavaScript to run this app.</noscript>
