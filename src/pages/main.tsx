e2063576c6053f929d5d494e06f16f3d8fd42dfd
jiasir <jiasir@icloud.com>
2023-05-13 22:48:37 +0800

    Update main content and reformat code

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -9,11 +9,12 @@ function Main(): JSX.Element {
     return (
         <main className="container">
             <div className="px-4 py-5 my-5 text-center">
-                <img className="img-thumbnail img-fluid d-block mx-auto mb-4" src="/sunsunChatLogo.png" alt="sunsun Chat" width="200" height="200"/>
+                <img className="img-thumbnail img-fluid d-block mx-auto mb-4" src="/sunsunChatLogo.png"
+                     alt="sunsun Chat" width="200" height="200"/>
                 <h1 className="display-5 fw-bold text-body-emphasis">sunsun-Chat</h1>
                 <div className="col-lg-6 mx-auto">
                     <p className="lead mb-4">
-                        sunsun-Chat is an AI chat assistant, chatGPT out of the box, no need to apply for an account and configuration.
+                        sunsun-Chat is an AI chat assistant, chatGPT out of the box, no account and proxy required.
                     </p>
                     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                         <button type="button" className="btn btn-primary btn-lg px-4 gap-3" onClick={handleClick}>
