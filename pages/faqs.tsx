ddf19062b4a652161d053a172fb14425b37c7a59
jiasir <jiasir@icloud.com>
2023-05-16 20:57:14 +0800

    Update faqs typography

--- pages/faqs.tsx
+++ pages/faqs.tsx
@@ -1,18 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import * as React from "react";
-import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
-import {styled} from "@mui/material/styles";
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import {Trans, useTranslation} from "react-i18next";
-
-const Item = styled(Paper)(({theme}) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "left",
-    color: theme.palette.text.secondary,
-}));
 
 function Faqs(): JSX.Element {
     const {t} = useTranslation();
@@ -20,62 +9,56 @@ function Faqs(): JSX.Element {
     return (
         <main className="container">
             <div className="px-4 py-5 my-5 text-center">
-                <h1 className="display-5 fw-bold text-body-emphasis mx-auto mb-4">
+                <Typography variant="h3" component="h1" gutterBottom>
                     FAQs
-                </h1>
-                {/*<Trans i18nKey="title">{t("title")}</Trans>*/}
-                <div>
-                    <Box marginBottom={3}>
-                        <Stack direction="row" spacing={1}>
-                            <Item>
-                                <Trans i18nKey={"faqs.free_q"}>{t("faqs.free_q")}</Trans>
-                            </Item>
-                            <Item>
-                                <Trans i18nKey={"faqs.free_a"}>{t("faqs.free_a")}</Trans>
-                            </Item>
-                        </Stack>
-                    </Box>
-                    <Box marginBottom={3}>
-                        <Stack direction="row" spacing={1}>
-                            <Item>
-                                <Trans i18nKey={"faqs.accessible_q"}>{t("faqs.accessible_q")}</Trans>
-                            </Item>
-                            <Item>
-                                <Trans i18nKey={"faqs.accessible_a"}>{t("faqs.accessible_a")}</Trans>
-                            </Item>
-                        </Stack>
-                    </Box>
-                    <Box marginBottom={3}>
-                        <Stack direction="row" spacing={1}>
-                            <Item>
-                                <Trans i18nKey={"faqs.privacy_q"}>{t("faqs.privacy_q")}</Trans>
-                            </Item>
-                            <Item>
-                                <Trans i18nKey={"faqs.privacy_a"}>{t("faqs.privacy_a")}</Trans>
-                            </Item>
-                        </Stack>
-                    </Box>
-                    <Box marginBottom={3}>
-                        <Stack direction="row" spacing={1}>
-                            <Item>
-                                <Trans i18nKey={"faqs.gpt_q"}>{t("faqs.gpt_q")}</Trans>
-                            </Item>
-                            <Item>
-                                <Trans i18nKey={"faqs.gpt_a"}>{t("faqs.gpt_a")}</Trans>
-                            </Item>
-                        </Stack>
-                    </Box>
-                    <Box marginBottom={3}>
-                        <Stack direction="row" spacing={1}>
-                            <Item>
-                                <Trans i18nKey={"faqs.sunsun_chat_q"}>{t("faqs.sunsun_chat_q")}</Trans>
-                            </Item>
-                            <Item>
-                                <Trans i18nKey={"faqs.sunsun_chat_a"}>{t("faqs.sunsun_chat_a")}</Trans>
-                            </Item>
-                        </Stack>
-                    </Box>
-                </div>
+                </Typography>
+
+                <Box sx={{marginTop: 3, textAlign: "left"}}>
+                    <Typography variant="body1" component="div" sx={{marginBottom: 3}}>
+                        <Typography variant="h5" component="div" sx={{fontWeight: 'bold', marginBottom: 1}}>
+                            <Trans i18nKey={"faqs.free_q"}>{t("faqs.free_q")}</Trans>
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            <Trans i18nKey={"faqs.free_a"}>{t("faqs.free_a")}</Trans>
+                        </Typography>
+                    </Typography>
+
+                    <Typography variant="body1" component="div" sx={{marginBottom: 3}}>
+                        <Typography variant="h5" component="div" sx={{fontWeight: 'bold', marginBottom: 1}}>
+                            <Trans i18nKey={"faqs.accessible_q"}>{t("faqs.accessible_q")}</Trans>
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            <Trans i18nKey={"faqs.accessible_a"}>{t("faqs.accessible_a")}</Trans>
+                        </Typography>
+                    </Typography>
+
+                    <Typography variant="body1" component="div" sx={{marginBottom: 3}}>
+                        <Typography variant="h5" component="div" sx={{fontWeight: 'bold', marginBottom: 1}}>
+                            <Trans i18nKey={"faqs.privacy_q"}>{t("faqs.privacy_q")}</Trans>
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            <Trans i18nKey={"faqs.privacy_a"}>{t("faqs.privacy_a")}</Trans>
+                        </Typography>
+                    </Typography>
+
+                    <Typography variant="body1" component="div" sx={{marginBottom: 3}}>
+                        <Typography variant="h5" component="div" sx={{fontWeight: 'bold', marginBottom: 1}}>
+                            <Trans i18nKey={"faqs.gpt_q"}>{t("faqs.gpt_q")}</Trans>
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            <Trans i18nKey={"faqs.gpt_a"}>{t("faqs.gpt_a")}</Trans>
+                        </Typography>
+                    </Typography>
+
+                    <Typography variant="body1" component="div" sx={{marginBottom: 3}}>
+                        <Typography variant="h5" component="div" sx={{fontWeight: 'bold', marginBottom: 1}}>
+                            <Trans i18nKey={"faqs.sunsun_chat_q"}>{t("faqs.sunsun_chat_q")}</Trans>
+                        </Typography>
+                        <Typography variant="body1" component="div">
+                            <Trans i18nKey={"faqs.sunsun_chat_a"}>{t("faqs.sunsun_chat_a")}</Trans>
+                        </Typography>
+                    </Typography>
+                </Box>
             </div>
         </main>
     );
