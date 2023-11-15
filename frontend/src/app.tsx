1f629d0bd1d4ebe75248d4df3d792f45e5be54d1
taiojia <taio@outlook.com>
2023-11-15 13:18:33 +0800

    Add react-ga package and initialize Google Analytics tracking.

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,10 +1,16 @@
+import ReactGA from 'react-ga';
 import 'bootstrap/dist/css/bootstrap.css'
-import Main from './main';
+import MainComponent from './main';
+
+if (typeof window !== 'undefined') {
+  ReactGA.initialize('G-C8X75XBX0J');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 function App(): JSX.Element {
     return (
         <div className="container">
-            <Main/>
+            <MainComponent/>
         </div>
     );
 }
